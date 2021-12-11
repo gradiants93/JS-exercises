b7c635b4f63ab1dd8c49240d5b89395542928d16
gradiants93 <75824980+gradiants93@users.noreply.github.com>
2021-12-11 12:06:40 -0800

    Exercise 11

--- loops.js
+++ loops.js
@@ -160,17 +160,18 @@ inside 1
 *********************************** 
 */
 
-//for (let outsideCounter = 0; outsideCounter <= 3 /*???*/; ) {
-//  console.log("counting down from", outsideCounter);
-//  for (let insideCounter = outsideCounter /*???*/ /*???*/; ; ) {
-//    console.log("inside" /*???*/);
-//  }
-//  console.log("***********************************");
-//}
+for (let outsideCounter = 0; outsideCounter <= 3 /*???*/; outsideCounter++) {
+  console.log("counting down from", outsideCounter);
+  for (let insideCounter = outsideCounter; insideCounter > 0 /*???*/ /*???*/; insideCounter-- ) {
+    console.log("inside " + insideCounter/*???*/);
+  }
+  console.log("***********************************");
+}
 
 // Congrats, you made it to the end!
 // Did you find this easy or hard? If you used references, which ones helped you?
 // Please answer in a comment below.
-//
+// I did this before learning about do while loops so that threw me off a little but I looked up the official ref for that.
+// Other than that it was pretty easy. The only weird thing was when I tried my loops in the node.js console, it kept doubling my last value but when I ran the file it didn't do that
 
 // Email your file to us or commit your file to GitHub and email us a link.
